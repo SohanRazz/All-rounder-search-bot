2a0bf0aae991de973a2c88529b0f9318de788802
SohanRazz <121212504+SohanRazz@users.noreply.github.com>
2022-12-27 22:49:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Forward bot')
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001852774435))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'SOHANRAJPUROHIT')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
