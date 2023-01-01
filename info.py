a19bf61b9140b744d3e7b3d3e046977b5d76a421
SohanRazz <121212504+SohanRazz@users.noreply.github.com>
2023-01-01 21:30:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('https://t.me/netflix_hindi_movies_4k')
 auth_grp = environ.get('https://t.me/netflix_hindi_movies_4k')
-AUTH_CHANNEL = int(https://t.me/netflix_hindi_movies_4k) if auth_channel and id_pattern.search(auth_channel) else None
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
